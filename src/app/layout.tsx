74c13948b5225f0e39a8bc35b09fc456dcb6cb9e
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-05-28 01:18:11 +0530

    fix: description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "LLocal",
-  description: "",
+  description: "An application to utilize locally hosted LLM's",
 };
 
 export default function RootLayout({
