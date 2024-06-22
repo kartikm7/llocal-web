e3db3a4879a9a7ce6e8d8d391f786b437958cf8a
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-06-22 23:38:27 +0530

    feat: added button for contributions

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import { Separator } from "./ui/separator";
 import { twMerge } from "tailwind-merge";
 import Link from "next/link";
 import { FaDiscord, FaGithub } from "react-icons/fa";
+import { HiOutlineExternalLink } from "react-icons/hi";
 
 export const FooterLayout = ({
   className,
@@ -21,12 +22,28 @@ export const Footer = () => {
     <FooterLayout className="flex flex-col w-full px-10">
       <div className="flex border-t p-5 justify-between items-center">
         <div className="text-xs">
-          <h1 className="font-semibold md:text-xl opacity-75 hover:opacity-100 transition-all cursor-pointer">llocal.in</h1>
-          <h1>brought you by <Link href={'https://twitter.com/codewithkt'} target="_parent"><span className="font-semibold underline">Kartikeya Mishra.</span></Link></h1>
+          <h1 className="font-semibold md:text-xl opacity-75 hover:opacity-100 transition-all cursor-pointer">
+            llocal.in
+          </h1>
+          <h1>
+            brought you by{" "}
+            <Link href={"https://twitter.com/codewithkt"} target="_parent">
+              <span className="font-semibold underline">Kartikeya Mishra.</span>
+            </Link>
+          </h1>
         </div>
-        <div className="flex gap-2 text-2xl">
-          <Link href={"https://github.com/kartikm7/llocal"}><FaGithub className="opacity-50 hover:opacity-100 hover:scale-95 transition-all" /></Link>
-          <Link href={`${process.env.NEXT_PUBLIC_DISCORD}`} target="_blank"><FaDiscord className="opacity-50 hover:opacity-100 hover:scale-95 transition-all" /></Link>
+        <div className="flex flex-col justify-end items-end gap-1">
+          <div className="flex gap-2 text-2xl">
+            <Link href={"https://github.com/kartikm7/llocal"}>
+              <FaGithub className="opacity-50 hover:opacity-100 hover:scale-95 transition-all" />
+            </Link>
+            <Link href={`${process.env.NEXT_PUBLIC_DISCORD}`} target="_blank">
+              <FaDiscord className="opacity-50 hover:opacity-100 hover:scale-95 transition-all" />
+            </Link>
+          </div>
+          <Link href={"https://github.com/kartikm7/llocal-web"} target="_blank">
+            <span className="text-xs flex justify-center items-center opacity-50 hover:opacity-100 hover:scale-95 transition-all">Want to fix something here? <HiOutlineExternalLink className="ml-1" /></span>
+          </Link>
         </div>
       </div>
     </FooterLayout>
