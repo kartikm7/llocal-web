e9f5ded602b55670e3b9060d17e809411caa1867
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-06-25 20:48:29 +0530

    refactor: recongized b y ollama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           titleComponent={
             <div className="text-center flex flex-col justify-center items-center gap-5 md:mb-20">
               <AnimatedGradientText className="text-xs p-1 md:p-2">
-                ✨ Officially supported by Ollama!
+                ✨ Officially recognized by Ollama!
               </AnimatedGradientText>
               <div>
                 <h1 className="font-light text-8xl">llocal</h1>
