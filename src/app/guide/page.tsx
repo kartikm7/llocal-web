95bc785d08c123f3c9af2a94ed4e6f5ddc3eb444
Kamakshya Nanda <91257779+kamakshyan@users.noreply.github.com>
2024-09-11 16:44:56 -0500

    Fix typo in escape character
    Corrected a typo in the escape character from &abos; to &apos;.

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -14,7 +14,7 @@ export default function Guide(){
       <li>- If you&apos;ve not installed Ollama, let LLocal guide you on that. Follow each step prompted, and LLocal will download the installer for you. This would take about 2 minutes.</li>
       <li>- Now that, LLocal and Ollama are both setup you can, start downloading models. This can be done through the <span className="text-orange-400">Pull a model section in settings</span>, which can be accessed through the command centre of the sidebar.</li>
       <li>- There are some recommendations below the Pull a model section, but I feel a general consumer should have slightly more knowledge on what model is correct for them.<span className="text-orange-400"> If you are not interested and just want to get going, then pull phi3 or gemma:2b</span>.</li>
-      <li>- So the model you use llocally depends a lot more on your machine than your choice, so if you have a dedicated graphics card with 6-8GB of VRAM you can make use of models around 7-8B parameters if you&abos;re machine has no VRAM or lesser than 6GB I would highly recommend models with 3.5B parameters and lesser to be run <br /> (like tinyllama, gemma:2b, phi3, etc.).</li>
+      <li>- So the model you use llocally depends a lot more on your machine than your choice, so if you have a dedicated graphics card with 6-8GB of VRAM you can make use of models around 7-8B parameters if you&apos;re machine has no VRAM or lesser than 6GB I would highly recommend models with 3.5B parameters and lesser to be run <br /> (like tinyllama, gemma:2b, phi3, etc.).</li>
       <li>- You can find out about the supported models via <Link href="https://www.ollama.ai/library" target="_blank">Ollama.ai</Link></li>
       <li>- Voila! You&apos;re all set now!</li>
     </ul>
