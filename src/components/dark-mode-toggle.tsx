a36cf245f9209a4759a0aa9a233af1f4581cf915
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-07-12 01:21:11 +0530

    fix: inconsistent styling for button

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" className="bg-transparent border border-foreground opacity-75 hover:opacity-100 hover:bg-transparent border-opacity-20 rounded-sm" size="icon">
           <LuSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MdOutlineDarkMode className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
